c4ab45566f9b4d70fc013b0f7d6ea1d6d9ad8ecd
bhaskar07 <bb7talla@gmail.com>
2023-09-04 16:23:03 +0530

    Update Square.js

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,29 +1,35 @@
-import React from "react"
-import styled from "styled-components"
+import React from 'react'
+import styled from 'styled-components'
+import circle from './../assets/circle.png'
 
 const Box = styled.button`
-  max-width: 80px;
-  max-height: 80px;
-  width: 100%;
-  height: 100%;
+  ${'' /* max-width: 80px; */}
+  ${'' /* max-height: 80px; */}
+  width: 60px;
+  height: 60px;
   outline: none;
   font-family: fantasy;
-  font-size: 32px;
+  ${'' /* font-size: 32px; */}
   border: 1px solid aquamarine;
-  background-color: ${({ isValid }) => (isValid ? "darkorange" : "beige")};
+  background-color: ${({ isValid }) => (isValid ? 'darkorange' : 'beige')};
   &:active {
     font-size: 40px;
   }
 `
 
 const Square = (props) => {
+  console.log('Square props', props)
   return (
     <Box
       isValid={props.isValidPos}
       id={props.id}
       onClick={() => props.onClick()}
     >
-      {props.isEmpty}
+      {props.isEmpty === 'O' ? (
+        <img height='60' width='60' src={circle} alt='react logo' />
+      ) : (
+        ''
+      )}
     </Box>
   )
 }
